fcaaf76bf3a7d2ec8b63e52a7920d65d71bfb69b
zulfiker62 <zulfikerali62783@gmail.com>
2025-05-14 17:10:07 +0600

    Update01 app.js

--- tanzu-sample-app3/app.js
+++ tanzu-sample-app3/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Tanzu Kubernetes! update-1');
+  res.send('Hello from Tanzu Kubernetes! update-02');
 });
 
 app.listen(port, () => {
